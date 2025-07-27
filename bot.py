0435e72b80350950359b129ec57f9e187b013bc3
pimek5 <pimekpimek42@gmail.com>
2025-07-27 12:29:04 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -207,5 +207,4 @@ async def on_voice_state_update(member, before, after):
 import os
 
 bot.run(os.getenv("DISCORD_TOKEN"))
-bot.run(MTI3NDI3NjExMzY2MDY0NTM4OQ.G2sDqW.X1vAPGYzNpdk3KtBDyRnNonHutFVXsedr0aC-c)
 
