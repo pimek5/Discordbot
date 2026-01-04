c1b41f5015359a04bd9ce6d72d4a1501bf0fcdd5
pimek5 <pimekpimek42@gmail.com>
2026-01-04 08:12:47 +0100

    revert: remove Agurin from manual list - will test if in API first

--- tracker/HEXBET/pro_players.py
+++ tracker/HEXBET/pro_players.py
@@ -30,7 +30,6 @@ KNOWN_PRO_PLAYERS = {
     'Tactical', 'Ssumday', 'River', 'Tenacity', 'Busio', 'Dhokla', 'Massu',
     # Other notable
     'Rekkles', 'Perkz', 'Alphari', 'Jensen', 'PowerOfEvil', 'Svenskeren',
-    'Agurin',  # Popular EUW jungler/streamer
 }
 
 async def load_pro_players_from_api():
